3006c410b3e93711205b0158ccdf296ae8e664f1
tartaric_acid <baka943@qq.com>
2023-09-26 14:56:37 +0800

    修正 action

--- replace.js
+++ replace.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 fs.readFile("tlm-utils.js", "utf-8", (error, data) => {
+    console.log(typeof data)
     let out = data.replaceAll("__vue_component__", "__vue_component__tlm")
         .replaceAll("__vue_script__", "__vue_script__tlm")
         .replaceAll("__vue_render__", "__vue_render__tlm")
